9a0b1e24819a8e6a8d462dd5b2864c73280346f0
Weslei Rafael <rafael_velao@hotmail.com>
2024-01-11 00:46:11 -0300

    Começando a imprementar um metodo que imprimi o tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -21,5 +21,7 @@ namespace tabuleiro
             this.qtdMovimentos = 0;
             
         }
+  
+        
     }
 }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -6,17 +6,22 @@ using System.Threading.Tasks;
 
 namespace tabuleiro
 {
-   class Tabuleiro
+    class Tabuleiro
     {
         public int linhas { get; set; }
         public int colunas { get; set; }
-        private Peca[,] peca;
-        
+        private Peca[,] pecas;
+
         public Tabuleiro(int linhas, int colunas)
         {
             this.linhas = linhas;
             this.colunas = colunas;
-            peca = new Peca[linhas,colunas];    
+            pecas = new Peca[linhas, colunas];
+        }
+        public Peca peca(int linhas, int colunas)
+        {
+            return pecas[linhas, colunas];
+
         }
     }
 }
